79ea5439aedb33fa0033d5fa9e91a8a5c85c6fdc
YOGI MEIZAL PUTRA <yogimeizalputra@gmail.com>
2023-09-19 07:40:53 +0700

    Adding Hotel Slide Number

--- src/pages/hotel/Hotel.jsx
+++ src/pages/hotel/Hotel.jsx
@@ -2,41 +2,64 @@ import './hotel.css'
 import Header from '../../components/header/Header'
 import MailList from '../../components/mailList/MailList'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faLocationDot } from '@fortawesome/free-solid-svg-icons'
+import { faCircleArrowLeft, faCircleArrowRight, faCircleXmark, faLocationDot } from '@fortawesome/free-solid-svg-icons'
+import { useState } from 'react'
 
 const Hotel = () => {
 
+    const [slideNumber, setSlideNumber] = useState(0);
+    const [open, setOpen] = useState(false);
+
     const photos = [
         {
-            id: 1,
             src: "https://cf.bstatic.com/xdata/images/hotel/max1280x900/261707778.jpg?k=56ba0babbcbbfeb3d3e911728831dcbc390ed2cb16c51d88159f82bf751d04c6&o=&hp=1",
         },
         {
-            id: 2,
             src: "https://cf.bstatic.com/xdata/images/hotel/max1280x900/261707367.jpg?k=cbacfdeb8404af56a1a94812575d96f6b80f6740fd491d02c6fc3912a16d8757&o=&hp=1",
         },
         {
-            id: 3,
             src: "https://cf.bstatic.com/xdata/images/hotel/max1280x900/261708745.jpg?k=1aae4678d645c63e0d90cdae8127b15f1e3232d4739bdf387a6578dc3b14bdfd&o=&hp=1",
         },
         {
-            id: 4,
             src: "https://cf.bstatic.com/xdata/images/hotel/max1280x900/261707776.jpg?k=054bb3e27c9e58d3bb1110349eb5e6e24dacd53fbb0316b9e2519b2bf3c520ae&o=&hp=1",
         },
         {
-            id: 5,
             src: "https://cf.bstatic.com/xdata/images/hotel/max1280x900/261708693.jpg?k=ea210b4fa329fe302eab55dd9818c0571afba2abd2225ca3a36457f9afa74e94&o=&hp=1",
         },
         {
-            id: 6,
             src: "https://cf.bstatic.com/xdata/images/hotel/max1280x900/261707389.jpg?k=52156673f9eb6d5d99d3eed9386491a0465ce6f3b995f005ac71abc192dd5827&o=&hp=1",
         },
     ]
+
+    const handleOpen = (i) => {
+        setSlideNumber(i);
+        setOpen(true);
+    };
+
+    const handleMove = (direction) => {
+        let newSlideNumber;
+
+        if(direction === "l") {
+            newSlideNumber = slideNumber === 0 ? 5 : slideNumber - 1;
+        } else {
+            newSlideNumber = slideNumber === 5 ? 0 : slideNumber + 1;
+        }
+
+        setSlideNumber(newSlideNumber)
+    }
 
     return (
         <div>
             <Header type='list' />
             <div className="hotelContainer">
+                { open && <div className="slider">
+                    <FontAwesomeIcon icon={faCircleXmark} className='close' onClick={() => setOpen(false)}/>
+                    <FontAwesomeIcon icon={faCircleArrowLeft} className='arrow' onClick={() => handleMove("l")}/>
+                    <div className="sliderWrapper">
+                        <img src={photos[slideNumber].src} alt="" className="sliderImg" />
+                    </div>
+                    <FontAwesomeIcon icon={faCircleArrowRight} className='arrow' onClick={() => handleMove("r")}/>
+                </div>}
                 <div className="hotelWrapper">
                     <button className="bookNow">Reserve or Book Now!</button>
                     <h1 className='hotelTitle'>Grand Hotel</h1>
@@ -52,8 +75,8 @@ const Hotel = () => {
                     </span>
                     <div className="hotelImages">
                         {
-                            photos.map(photo => (
-                                <div className="hotelImgWrapper" key={photo.id}>
+                            photos.map((photo, i) => (
+                                <div className="hotelImgWrapper" key={i} onClick={() => handleOpen(i)}>
                                     <img src={photo.src} alt="" className="hotelImg" />
                                 </div>
                             ))
